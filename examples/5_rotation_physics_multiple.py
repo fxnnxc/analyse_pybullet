b51b44d7a9699883c49fc9b0bdd1c8366cf7158d
Bumjin Park <bumjin@kaist.ac.kr>
2021-03-03 16:53:02 +0900

    update orbit

--- examples/5_rotation_physics_multiple.py
+++ examples/5_rotation_physics_multiple.py
@@ -2,7 +2,7 @@ import pybullet as p
 import pybullet_data
 import time 
 import numpy as np 
-
+import random 
 #==============================================
 # === Perfect Circle Rotation multiple case ===
 #==============================================
@@ -12,16 +12,20 @@ p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 # === hyper parameters ===
-num_agents = 200
+num_agents = 300
 radius = [np.random.random()*1+3 for i in range(num_agents)]
 theta =  [np.random.random()* np.pi*2 for i in range(num_agents)] 
 period = [np.random.randint(100,1000) for i in range(num_agents)]
 
+colors = [[1, 1,0], [255/255,192/255,203/255], [255/255, 153/255, 255/255], [255/255, 204/255, 0]]
+
 # === create object ===
 target_pos = [[radius[i] * np.cos(theta[i]), radius[i]* np.sin(theta[i])] for i in range(num_agents)]
-target = [p.loadURDF("cube_small.urdf", target_pos[i]+[0], globalScaling=3) for i in range(num_agents)]
+target = [p.loadURDF("cube_small.urdf", target_pos[i]+[0], globalScaling=np.random.random()/2+1) for i in range(num_agents)]
 for t in target:
-    p.changeVisualShape(t, -1, rgbaColor=[np.random.random(), np.random.random(),np.random.random(), 1])
+    #p.changeVisualShape(t, -1, rgbaColor=[np.random.random(), np.random.random(),np.random.random(), 1])
+    p.changeVisualShape(t, -1, rgbaColor=random.choice(colors)+[random.random()/2+0.5])
+    
     p.changeDynamics(t, -1, mass=1)
 p.setTimeStep(1)
 
